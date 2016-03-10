c2f6d313288ffa8263d7630cad45f3e372f744d4
Brandon Stookey <bwstookey@gmail.com>
2016-03-10 14:13:49 -0600

    Authentiation up and running

--- public/client/app/app.routes.js
+++ public/client/app/app.routes.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular.module('dreamjournal', [
   'ngRoute',
   'dreamjournal.login',
@@ -8,9 +6,12 @@ angular.module('dreamjournal', [
   'dreamjournal.profile',
   'dreamjournal.viewPost',
   'dreamjournal.newPost',
-  'dreamjournal.home'          
+  'dreamjournal.home',
+  'auth0',
+  'angular-storage',
+  'angular-jwt'          
 ])
-.config(function($routeProvider, $locationProvider) {
+.config(['$routeProvider', '$httpProvider', 'authProvider', 'jwtInterceptorProvider', function($routeProvider, $httpProvider, authProvider, jwtInterceptorProvider) {
   $routeProvider
     .when('/login', {
       templateUrl: 'app/shared/login.view.html',
@@ -22,22 +23,68 @@ angular.module('dreamjournal', [
     })
     .when('/graph', {
       templateUrl: 'app/shared/graph.view.html',
-      controller: 'graphController'
+      controller: 'graphController',
+      requiresLogin: true
     })
     .when('/profile', {
       templateUrl: 'app/shared/profile.view.html',
-      controller: 'profileController'
+      controller: 'profileController',
+      requiresLogin: true      
     })
     .when('/viewPost', {
       templateUrl: 'app/shared/view.post.view.html',
-      controller: 'viewPostController'
+      controller: 'viewPostController',
+      requiresLogin: true      
     })
     .when('/newPost', {
       templateUrl: 'app/shared/write.new.post.view.html',
-      controller: 'newPostController'
+      controller: 'newPostController',
+      requiresLogin: true      
     })
     .when('/', {
       templateUrl: 'app/shared/home.view.html',
-      controller: 'homeController'
+      controller: 'homeController',
+      requiresLogin: true      
     })        
+}])
+.config(['authProvider', 'jwtInterceptorProvider', '$httpProvider', function (authProvider, jwtInterceptorProvider, $httpProvider) {
+  // https://manage.auth0.com/#/applications/1lZ3sYfpkqI5yJkeFXYscvLsR7dnG7q2/quickstart Just In case I need a refresher
+  authProvider.init({
+    domain: 'dreamjournal.auth0.com',
+    clientID: '1lZ3sYfpkqI5yJkeFXYscvLsR7dnG7q2',
+    callbackURL: location.href,    
+    loginUrl: '/login'
+      // We're annotating this function so that the `store` is injected correctly when this file is minified
+  });
+  jwtInterceptorProvider.tokenGetter = ['store', function(store) {
+    // Return the saved token
+    return store.get('token');
+  }];
+
+    $httpProvider.interceptors.push('jwtInterceptor');  
+}])
+.run(function($rootScope, auth, store, jwtHelper, $location) {
+  // This events gets triggered on refresh or URL change
+  $rootScope.$on('$locationChangeStart', function() {
+    var token = store.get('token');
+    if (token) {
+      if (!jwtHelper.isTokenExpired(token)) {
+        if (!auth.isAuthenticated) {
+          auth.authenticate(store.get('profile'), token);
+        }
+      } else {
+        // Either show the login page or use the refresh token to get a new idToken
+        $location.path('/login');
+      }
+    }
+  });
 });
+
+
+
+
+
+
+
+
+
