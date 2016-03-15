e7db5c592fe3df5b3a243155bea2f592853ea675
Brandon Stookey <bwstookey@gmail.com>
2016-03-15 13:35:08 -0500

    (feat)Added textAngular to writenewpost controller

--- public/client/app/shared/writeNewPost/write.new.post.controller.js
+++ public/client/app/shared/writeNewPost/write.new.post.controller.js
@@ -1,23 +1,23 @@
 'use strict';
 
-angular.module('dreamjournal.newPost', [])
+angular.module('dreamjournal.newPost', ['textAngular'])
 
 .controller('newPostController', ['$scope', '$http', 'auth', function ($scope, $http, auth) {
-  $scope.createPost = function(postTitle, post){
-	  var userName = auth.profile.name;
-    var userEmail = auth.profile.email;
+  // $scope.createPost = function(postTitle, post){
+	 //  var userName = auth.profile.name;
+  //   var userEmail = auth.profile.email;
 
-    return $http({
-      method: 'POST',
-      url: '/create/post',
-      data: {postTitle: postTitle, post: post, name: userName, email: userEmail}
-    })
-    .then(function(resp){
-      console.log(resp);
-    }, function(error){
-      console.log(error);
-    });
-  };
+  // //   return $http({
+  // //     method: 'POST',
+  // //     url: '/create/post',
+  // //     data: {postTitle: postTitle, post: post, name: userName, email: userEmail}
+  // //   })
+  // //   .then(function(resp){
+  // //     console.log(resp);
+  // //   }, function(error){
+  // //     console.log(error);
+  // //   });
+  // // };
 }]);
 
      
