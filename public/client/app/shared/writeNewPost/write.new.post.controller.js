c4bf5d4df65c268603202257d524532a4dfd3d11
Brandon Stookey <bwstookey@gmail.com>
2016-03-16 17:33:34 -0500

    (feat)Edit textAngular toolbar to include only relevant options

--- public/client/app/shared/writeNewPost/write.new.post.controller.js
+++ public/client/app/shared/writeNewPost/write.new.post.controller.js
@@ -19,6 +19,33 @@ angular.module('dreamjournal.newPost', ['textAngular'])
     });
   };
   
-}]);
-
-     
+}])
+.config(['$provide', function($provide){
+  //=============================================This Allows me to Edit the Toolbar, and below is the website with instructions
+  // https://github.com/fraywing/textAngular/wiki/Setting-Defaults
+        // this demonstrates how to register a new tool and add it to the default toolbar
+        $provide.decorator('taOptions', ['$delegate', function(taOptions){
+            // $delegate is the taOptions we are decorating
+            // here we override the default toolbars and classes specified in taOptions.
+            taOptions.forceTextAngularSanitize = true; // set false to allow the textAngular-sanitize provider to be replaced
+            taOptions.keyMappings = []; // allow customizable keyMappings for specialized key boards or languages
+            taOptions.toolbar = [
+                ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'quote'],
+                ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo'],
+                ['justifyLeft','justifyCenter','justifyRight', 'justifyFull'],
+                ['insertImage', 'insertLink', 'wordcount']
+                //Later if I want to insertImage just place 'insertImage' insertLink
+            ];
+            taOptions.classes = {
+                focussed: 'focussed',
+                toolbar: 'btn-toolbar',
+                toolbarGroup: 'btn-group',
+                toolbarButton: 'btn btn-default',
+                toolbarButtonActive: 'active',
+                disabled: 'disabled',
+                textEditor: 'form-control',
+                htmlEditor: 'form-control'
+            };
+            return taOptions; // whatever you return will be the taOptions
+        }]);
+    }]);
