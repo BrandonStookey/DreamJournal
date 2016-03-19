5bb5a96afeccc7ba4d9e3ae5b60c09173d8bfd35
Brandon Stookey <bwstookey@gmail.com>
2016-03-18 20:28:44 -0500

    (fix)Attempt 1 to fix travis

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,57 +1,57 @@
-// var chai = require('chai')
-// var app = require('./../server/server');
-// var expect = chai.expect;
-// var request = require('supertest');
-// var helpers = require('./../server/helpers.js');
+var chai = require('chai')
+var app = require('./../server/server');
+var expect = chai.expect;
+var request = require('supertest');
+var helpers = require('./../server/helpers.js');
 
-// describe('', function () {
+describe('', function () {
 
-// //================================================================Test Add New User To Database=====================================================================
+//================================================================Test Add New User To Database=====================================================================
 
-// describe('http://localhost:4568/create/new/user', function () {
-//     describe('POST', function () {
+describe('http://localhost:4568/create/new/user', function () {
+    describe('POST', function () {
  
-//       var newUser = {
-//         name: 'Josh',
-//         email: 'josh@josh.io'
-//       };    
-//       it('responds with a 200 (ok) when a user is created', function (done) {
+      var newUser = {
+        name: 'Josh',
+        email: 'josh@josh.io'
+      };    
+      it('responds with a 200 (ok) when a user is created', function (done) {
 
-//         request(app)
-//           .post('/create/new/user')
-//           .send(newUser)
-//           .expect(200, done);
-//       });
-//   	});
-//   });
+        request(app)
+          .post('/create/new/user')
+          .send(newUser)
+          .expect(200, done);
+      });
+  	});
+  });
 
-// //==================================================================Test Get all Posts Request===================================================================
-//   describe('http://localhost:4568/get/all/posts', function () {
+//==================================================================Test Get all Posts Request===================================================================
+  describe('http://localhost:4568/get/all/posts', function () {
     
-//     describe('GET', function () {
+    describe('GET', function () {
     
-//       it('Retrieve all posts, responds with a 200 (OK)', function (done) {
-//         request(app)
-//           .get('/get/all/posts')
-//           .expect(200, done);
-//       });
-//     });
-//   });
+      it('Retrieve all posts, responds with a 200 (OK)', function (done) {
+        request(app)
+          .get('/get/all/posts')
+          .expect(200, done);
+      });
+    });
+  });
 
 
-// //======================================================================Test Get all User Posts========================================================================
+//======================================================================Test Get all User Posts========================================================================
 
-// describe('http://localhost:4568/get/all/user/posts', function () {
+describe('http://localhost:4568/get/all/user/posts', function () {
  
-//     describe('POST', function () {
+    describe('POST', function () {
   
-//       it('Retrieves all posts from a single user, responds with a 200 (OK)', function (done) {
-//         request(app)
-//           .post('/get/all/user/posts')
-//           .send('hatter@hatter.com')
-//           .expect(200, done);
-//       });
-//   	});
-//   });
-// });
+      it('Retrieves all posts from a single user, responds with a 200 (OK)', function (done) {
+        request(app)
+          .post('/get/all/user/posts')
+          .send('hatter@hatter.com')
+          .expect(200, done);
+      });
+  	});
+  });
+});
 
