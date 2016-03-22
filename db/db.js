f5817d97c8fabfef12eaa2e7e0cc5266d96dd095
Brandon Stookey <bwstookey@gmail.com>
2016-03-22 16:23:20 -0500

    (feat)Add comment box on home view posts

--- db/db.js
+++ db/db.js
@@ -20,11 +20,26 @@ var userSchema = mongoose.Schema({
       post: String,
       name: String,
       email: String,
-      dreamType: String, //0 === noDream , 1 === Dream, 2 === Nightmare
-      date: { type: Date, default: Date.now }
-    }]
+      dreamType: String,
+      date: { type: Date, default: Date.now },
+      comment: { 
+        type: [{
+        userEmail: String,
+        comment: String
+      }]},
+      like: { 
+        type: [{
+          userEmail: String,
+          like: Boolean
+      }]},
+    }],
   },
+  friendList:{ 
+    type: [{
+      userEmail: String
+    }]},
 });
+
 
 var User = mongoose.model('User', userSchema);
 
@@ -32,42 +47,6 @@ var User = mongoose.model('User', userSchema);
 module.exports.User = User;
 
 
-// var userSchema = mongoose.Schema({
-//   email: {
-//     type: String,
-//     unique: true,
-//     required: true
-//   },
-//   name: {
-//     type: String,
-//   },
-//   date: { type: Date, default: Date.now },   
-//   post: {
-//     type: [{
-//       postTitle: String,
-//       postDate: String,
-//       post: String,
-//       name: String,
-//       email: String,
-//       dreamType: String, //0 === noDream , 1 === Dream, 2 === Nightmare
-//       date: { type: Date, default: Date.now },
-//       comment: { 
-//         type: [{
-//         userEmail: String,
-//         comment:String
-//       }]},
-//       like: { 
-//         type: [{
-//           userEmail: String,
-//           like: Boolean
-//       }]},
-//     }],
-//   friendList:{ 
-//     type: [{
-//       userEmail: String
-//     }]},
-//   }
-// });
 
 
 
